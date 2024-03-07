9cc24e903ea50397e3f81fc1e5b54af1a13bf06b
pancawind <pcalvin0205@gmail.com>
2024-03-07 16:54:27 -0500

    Questions 1 to 4

---
+++ Q1.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define(solve-quadratic a b c)
+  (let ((discriminant (- (* b b) (* 4 a c))))
+    (cond ((> discriminant 0)
+        (list (/(round(*(/(+ (- b)(sqrt discriminant))(* 2 a)) 1000))1000) (/(round(*(/(- (- b) (sqrt discriminant))(* 2 a)) 1000))1000)))
+          ((= discriminant 0)
+           (list (/(round(*(/(+ (- b)(sqrt discriminant))(* 2 a)) 1000))1000)))
+          (else '()))))
+
+(display "(solve-quadratic 4 5 -10) ==> '")
+(display (solve-quadratic 4 5 -10))
+(newline)
+(display "(solve-quadratic 1 -2 1) ==> '")
+(display (solve-quadratic 1 -2 1))
+(newline)
+(display "(solve-quadratic 3 2 1) ==> '")
+(display (solve-quadratic 3 2 1))
---
+++ Q4.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+(define (heap-sort vec)
+  (define (heapify vec n i)
+    (let* ((largest i)
+           (left (+ (* 2 i) 1))
+           (right (+ (* 2 i) 2)))
+      (when (and (< left n) (> (vector-ref vec left) (vector-ref vec largest)))
+        (set! largest left))
+      (when (and (< right n) (> (vector-ref vec right) (vector-ref vec largest)))
+        (set! largest right))
+      (when (not (= largest i))
+        (let ((temp (vector-ref vec i)))
+          (vector-set! vec i (vector-ref vec largest))
+          (vector-set! vec largest temp)
+          (heapify vec n largest)))))
+
+  (define (build-heap vec)
+    (let ((n (vector-length vec)))
+      (do ((i (quotient n 2) (- i 1)))
+          ((< i 0) vec)
+        (heapify vec n i))))
+
+  (define (sort vec)
+    (let ((mutable-vec (vector-copy vec))
+          (n (vector-length vec)))
+      (build-heap mutable-vec)
+      (do ((i (- n 1) (- i 1)))
+          ((< i 0) mutable-vec)
+        (let ((temp (vector-ref mutable-vec 0)))
+          (vector-set! mutable-vec 0 (vector-ref mutable-vec i))
+          (vector-set! mutable-vec i temp)
+          (heapify mutable-vec i 0)))))
+
+  (sort vec))
+
+(display (heap-sort #(10 18 3 22 9 31 45 28))) ; Output: #(3 9 10 18 22 28 31 45)
+(newline)
+
+(define (insertion-sort vec)
+  (define (insert val vec)
+    (cond ((null? vec) (list val))
+          ((<= val (car vec)) (cons val vec))
+          (else (cons (car vec) (insert val (cdr vec))))))
+  
+  (define (insertion-sort-helper vec sorted)
+    (if (null? vec)
+        sorted
+        (insertion-sort-helper (cdr vec) (insert (car vec) sorted))))
+  
+  (apply vector (insertion-sort-helper (vector->list vec) '())))
+
+;; Example usage:
+(display (insertion-sort '#(10 18 3 22 9 31 45 28)))
+
+
+
+
+
+
+
